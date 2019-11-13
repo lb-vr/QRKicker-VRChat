8fa3610b96f8bb13e6a8bba0cf4bcf8e74581617
Shirao Shotaro <shirao.shotaro@gmail.com>
2019-11-14 00:45:23 +0900

    version0.0.0 beta commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace QRKicker {
+	partial class Form1 {
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent() {
+			this.tbLog = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.tbDirPath = new System.Windows.Forms.TextBox();
+			this.fsw = new System.IO.FileSystemWatcher();
+			this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.fsw)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tbLog
+			// 
+			this.tbLog.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.tbLog.Location = new System.Drawing.Point(0, 57);
+			this.tbLog.Multiline = true;
+			this.tbLog.Name = "tbLog";
+			this.tbLog.ReadOnly = true;
+			this.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.tbLog.Size = new System.Drawing.Size(462, 122);
+			this.tbLog.TabIndex = 0;
+			this.tbLog.WordWrap = false;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.AutoSize = true;
+			this.groupBox1.Controls.Add(this.linkLabel1);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.tbDirPath);
+			this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.groupBox1.Location = new System.Drawing.Point(0, 0);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(462, 57);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "フォルダ設定";
+			// 
+			// linkLabel1
+			// 
+			this.linkLabel1.AutoSize = true;
+			this.linkLabel1.Location = new System.Drawing.Point(72, 42);
+			this.linkLabel1.Name = "linkLabel1";
+			this.linkLabel1.Size = new System.Drawing.Size(230, 12);
+			this.linkLabel1.TabIndex = 3;
+			this.linkLabel1.TabStop = true;
+			this.linkLabel1.Text = "https://github.com/lb-vr/QRKicker-VRChat";
+			this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 42);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(60, 12);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Github link";
+			// 
+			// button1
+			// 
+			this.button1.AutoSize = true;
+			this.button1.Location = new System.Drawing.Point(429, 18);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(27, 22);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "...";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// tbDirPath
+			// 
+			this.tbDirPath.BackColor = System.Drawing.SystemColors.Window;
+			this.tbDirPath.Location = new System.Drawing.Point(6, 20);
+			this.tbDirPath.Name = "tbDirPath";
+			this.tbDirPath.Size = new System.Drawing.Size(417, 19);
+			this.tbDirPath.TabIndex = 0;
+			this.tbDirPath.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+			// 
+			// fsw
+			// 
+			this.fsw.EnableRaisingEvents = true;
+			this.fsw.SynchronizingObject = this;
+			this.fsw.Created += new System.IO.FileSystemEventHandler(this.fsw_Created);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(462, 179);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.tbLog);
+			this.Name = "Form1";
+			this.Text = "QRKicker - v0.0.0 beta";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.fsw)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox tbLog;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.LinkLabel linkLabel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox tbDirPath;
+		private System.IO.FileSystemWatcher fsw;
+		private System.Windows.Forms.FolderBrowserDialog fbd;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QRKicker {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+			System.Diagnostics.Process.Start("https://github.com/lb-vr/QRKicker-VRChat");
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e) {
+			startToWatch(tbDirPath.Text);
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			string path = System.Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\VRChat";
+			tbDirPath.Text = path;
+		}
+
+		private void startToWatch(string path) {
+			if (System.IO.Directory.Exists(path)) {
+				fsw.Path = path;
+				fsw.Filter = "VRChat*.png";
+				fsw.NotifyFilter = System.IO.NotifyFilters.FileName;
+				fsw.SynchronizingObject = this;
+				fsw.EnableRaisingEvents = true;
+				tbDirPath.BackColor = SystemColors.Window;
+				tbLog.AppendText("準備完了しました。\r\n");
+			}
+			else {
+				if (fsw.EnableRaisingEvents) {
+					tbLog.AppendText("存在しないディレクトリです。機能は無効です。\r\n");
+				}
+				fsw.EnableRaisingEvents = false;
+				tbDirPath.BackColor = SystemColors.Info;
+			}
+		}
+
+		private void fsw_Created(object sender, System.IO.FileSystemEventArgs e) {
+			tbLog.AppendText("新しい写真を検出しました。" + e.Name + "\r\n");
+			try {
+				System.IO.FileStream fs = new System.IO.FileStream(
+					e.FullPath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+				System.Drawing.Image img = System.Drawing.Image.FromStream(fs);
+				fs.Close();
+
+				ZXing.BarcodeReader reader = new ZXing.BarcodeReader();
+				var result = reader.Decode(new Bitmap(img));
+
+				// 結果のフェッチ
+				if (result != null) {
+					tbLog.AppendText("> QRコードが検出されました : " + result.Text + "\r\n");
+
+					// URLのチェック
+					Uri u = new Uri(result.Text);
+					if(u.Host.EndsWith(".booth.pm") && u.IsFile == false) {
+						System.Diagnostics.Process.Start(result.Text);
+					}
+					else {
+						tbLog.AppendText("> このQRコードは不正なリンク先です。\r\n");
+					}
+				}
+				else {
+					tbLog.AppendText("> QRコードは見つかりませんでした\r\n");
+				}
+			}
+			catch(System.IO.IOException ex) {
+				tbLog.AppendText("> IOException : " + ex.ToString() + "\r\n");
+			}
+			catch(System.InvalidOperationException ex) {
+				tbLog.AppendText("> InvalidOperationException : " + ex.ToString() + "\r\n");
+			}
+			catch(System.UriFormatException) {
+				tbLog.AppendText("> このQRコードは不正なURLです。\r\n");
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			if(fbd.SelectedPath == "") {
+				fbd.SelectedPath = System.Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+			}
+			var result = fbd.ShowDialog();
+			if(result == DialogResult.OK) {
+				tbDirPath.Text = fbd.SelectedPath;
+			}
+		}
+	}
+}
